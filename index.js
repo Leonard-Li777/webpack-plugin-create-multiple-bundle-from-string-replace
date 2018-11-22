ad0d6726c55c6fb759509a21a41cf2c1f8be27f1
lilun <lilun_cd@keruyun.com>
2018-11-22 16:57:12 +0800

    fix: image is repalced

--- index.js
+++ index.js
@@ -12,24 +12,21 @@ function ReplaceStringPatternPlugin(options = {}, config = {}) {
 }
 
 function replaceContent(result, [patterns = [], replacement = []]) {
-  if (typeof result === 'string') {
-    if (
-      Array.isArray(patterns) &&
-      Array.isArray(replacement) &&
-      patterns.length === replacement.length
-    ) {
-      return replaceOnce(result, patterns, replacement, 'g');
-    } else {
-      throw new Error(
-        `[webpack-plugin-create-multiple-bundle-from-string-replace]:
+  if (
+    Array.isArray(patterns) &&
+    Array.isArray(replacement) &&
+    patterns.length === replacement.length
+  ) {
+    return replaceOnce(result, patterns, replacement, 'g');
+  } else {
+    throw new Error(
+      `[webpack-plugin-create-multiple-bundle-from-string-replace]:
         options patterns/replacement must be an array and them length are equal, got 
         patterns = ${patterns}
         replacement = ${replacement}
         `,
-      );
-    }
+    );
   }
-  return result;
 }
 
 function writeFile({ distPath, env, file, result }) {
@@ -42,13 +39,27 @@ function replaceFile({ file, sourcePath, replacePattern }) {
   return replaceContent(fileContent, replacePattern);
 }
 
+function copyFile({ file, env = '', sourcePath, distPath }) {
+  if (
+    ['html', 'htm', 'css', 'js', 'jsx', 'json', 'md'].includes(
+      path.extname(file).replace('.', ''),
+    )
+  ) {
+    return true;
+  }
+
+  fs.copyFileSync(join(sourcePath, file), join(distPath, env, file));
+
+  return false;
+}
+
 ReplaceStringPatternPlugin.prototype.apply = function(compiler) {
   const { directReplace, options: replacePattern, config } = this;
   const sourcePath = config.sourcePath || compiler.options.output.path;
   const distPath =
     config.distPath ||
     `${compiler.options.output.path}/multiple-bundle-from-string-replace`;
-    
+
   const eventDone = (compilation, done) => {
     console.log(
       `[webpack-plugin-create-multiple-bundle-from-string-replace]: In ${distPath}`,
@@ -60,18 +71,22 @@ ReplaceStringPatternPlugin.prototype.apply = function(compiler) {
     });
     if (directReplace) {
       files.forEach(file => {
-        const result = replaceFile({ file, sourcePath, replacePattern });
-        writeFile({ distPath, file, result });
+        if (copyFile({ file, sourcePath, distPath })) {
+          const result = replaceFile({ file, sourcePath, replacePattern });
+          writeFile({ distPath, file, result });
+        }
       });
     } else {
       files.forEach(file => {
         for (const env in this.options) {
-          const result = replaceFile({
-            file,
-            sourcePath,
-            replacePattern: this.options[env],
-          });
-          writeFile({ distPath, env, file, result });
+          if (copyFile({ file, sourcePath, distPath, env })) {
+            const result = replaceFile({
+              file,
+              sourcePath,
+              replacePattern: this.options[env],
+            });
+            writeFile({ distPath, env, file, result });
+          }
         }
       });
     }
